cca0221fbc79b739dadb367425ff032e18139901
Khabarova.Elena <elkhab@mail.ru>
2019-02-26 16:48:53 +0300

    Добавлен пример на четность

---
+++ src/SecondExample.java
@@ -0,0 +1,12 @@
+public class SecondExample {
+
+public static void main(String[] args){
+        int n=5;
+        if(n%2==0){
+            System.out.println("Число "+n+" - четное");
+        }
+        else {
+            System.out.println("Число "+n+" - нечетное");
+        }
+    }
+}
